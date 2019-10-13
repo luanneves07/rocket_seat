586ad55c38fb8a6ebe6335864b05bba20fdf9a35
luanneves07 <luannevessilva@gmail.com>
2019-10-13 17:16:55 -0300

    #1 - Resolve exercicio 1

---
+++ javascript_starter/introducao_javascript.js
@@ -0,0 +1,12 @@
+console.log("Exercício 1")
+
+var endereco = {
+    rua: "Rua dos pinheiros",
+    numero: 1293,
+    bairro: "Centro",
+    cidade: "São Paulo",
+    uf: "SP"
+   };
+
+console.log(String.format('O usuário mora em {0} / {1}, no bairro {2}, na rua "{3}" com nº {4}.', 
+endereco.cidade, endereco.uf, endereco.bairro, endereco.rua, endereco.numero))
