3d8eac20466d03b0b9893b88df2fffc24dc2fc3c
luanneves07 <luannevessilva@gmail.com>
2019-10-13 17:40:54 -0300

    #1 Resolve exercicio 2

--- javascript_starter/introducao_javascript.js
+++ javascript_starter/introducao_javascript.js
@@ -10,3 +10,5 @@ var endereco = {
 
 console.log(String.format('O usuário mora em {0} / {1}, no bairro {2}, na rua "{3}" com nº {4}.', 
 endereco.cidade, endereco.uf, endereco.bairro, endereco.rua, endereco.numero))
+
+pares(10, 20)
--- javascript_starter/utils.js
+++ javascript_starter/utils.js
@@ -6,3 +6,15 @@ String.format = function() {
     }
     return s;
 }
+
+pares = function(x, y) {
+    pares = 0
+    for (target = x; target <= y; target++) {
+        //console.log(String.format("testando o numero -> {0}", target))
+        if (target % 2 == 0) {
+            console.log(String.format("Numero par encontrado: {0}", target))
+            pares++
+        }
+    }
+    console.log(String.format("Foram encontrados {0} numeros pares", pares))
+}
