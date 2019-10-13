c022b7c10134ea48792437430ae1f19107bc7408
luanneves07 <luannevessilva@gmail.com>
2019-10-13 17:53:07 -0300

    #1 Resolve execricio 3

--- javascript_starter/introducao_javascript.js
+++ javascript_starter/introducao_javascript.js
@@ -1,5 +1,4 @@
 console.log("Exercício 1")
-
 var endereco = {
     rua: "Rua dos pinheiros",
     numero: 1293,
@@ -11,4 +10,9 @@ var endereco = {
 console.log(String.format('O usuário mora em {0} / {1}, no bairro {2}, na rua "{3}" com nº {4}.', 
 endereco.cidade, endereco.uf, endereco.bairro, endereco.rua, endereco.numero))
 
-pares(10, 20)
+console.log("Exercício 2")
+console.log(pares(10, 20))
+
+console.log("Exercício 3")
+var skills = ["Javascript", "ReactJS", "React Native"];
+console.log(temHabilidade(skills));
--- javascript_starter/utils.js
+++ javascript_starter/utils.js
@@ -9,12 +9,18 @@ String.format = function() {
 
 pares = function(x, y) {
     pares = 0
+    resultado = ""
     for (target = x; target <= y; target++) {
         //console.log(String.format("testando o numero -> {0}", target))
         if (target % 2 == 0) {
-            console.log(String.format("Numero par encontrado: {0}", target))
+            resultado += (String.format("Numero par encontrado: {0}\n", target))
             pares++
         }
     }
-    console.log(String.format("Foram encontrados {0} numeros pares", pares))
+    resultado += (String.format("\n\n\nForam encontrados {0} numeros pares", pares))
+    return resultado
+}
+
+temHabilidade = function(skills) {
+    return skills.includes("Javascript")
 }
