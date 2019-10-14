c56344a4de2ab6cd0e5c9b5ae56a41d4ff5d5d35
luanneves07 <luannevessilva@gmail.com>
2019-10-14 02:33:41 -0300

    #2 - Resolve exercicio 2

--- javascript_starter/manipula_DOM.js
+++ javascript_starter/manipula_DOM.js
@@ -5,3 +5,5 @@ console.log("Exercício 1");
 btnElement.onclick = function() {
     container.appendChild(criaQuadrado(100));
 }
+
+console.log("Exercicio 2 -> Passe o mouse em cima do quadrado criado");
--- javascript_starter/manipula_helper.js
+++ javascript_starter/manipula_helper.js
@@ -8,6 +8,21 @@ criaQuadrado = function(px) {
     box.style.width = px;
     box.style.height = px;
     box.style.backgroundColor = '#F00';
- 
+    box.onmouseover = function() {
+        box.style.backgroundColor = recuperaNovaCor();
+    }
+
     return box;
+}
+
+/**
+ * Recupera uma cor aleatoria
+ */
+recuperaNovaCor = function() {
+    var letters = "0123456789ABCDEF";
+    var color = "#";
+    for (var i = 0; i < 6; i++) {
+        color += letters[Math.floor(Math.random() * 16)];
+    }
+    return color;
 }
